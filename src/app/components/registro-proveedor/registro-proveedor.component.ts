4cfd5e545b9b95d7da66f6f747e6f5d5925df992
Daniel <daniel.morave@campusucc.edu.co>
2021-04-24 14:47:53 -0500

    se realiza merge con la rama daniel

--- src/app/components/registro-proveedor/registro-proveedor.component.ts
+++ src/app/components/registro-proveedor/registro-proveedor.component.ts
@@ -1,17 +1,20 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-registro-proveedor',
   templateUrl: './registro-proveedor.component.html',
-  styleUrls: ['./registro-proveedor.component.scss']
+  styleUrls: ['./registro-proveedor.component.scss'],
 })
-export class RegistroProveedorComponent{
-
+export class RegistroProveedorComponent {
   /**
    * Declaración para poder manipular la información del formulario
    */
-  options: FormGroup;
+   options: FormGroup;
 
   /**
    * Se crea una instancia de la clase
@@ -23,7 +26,7 @@ export class RegistroProveedorComponent{
       phone: [null],
       cellPhone: [null, Validators.required],
       principalEmail: [null, Validators.required],
+      secodEmail: [null],
     });
   }
-
 }
