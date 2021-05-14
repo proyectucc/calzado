1a67b9325f020ec0138d7533e2a615458e197bb5
proyectucc <daniel.morave@campusuuc.edu.co>
2021-05-14 12:34:35 -0500

    listas de clientes

--- src/app/models/clientes.ts
+++ src/app/models/clientes.ts
@@ -2,22 +2,22 @@
  * Interface con los parámetros que tiene la Api de Clientes
  */
 
-//  export class Clientes {
-//     constructor(
-//       idCliente: number,
-//       primerNombre: string,
-//       segundoNombre: null | string,
-//       primerApellido: string,
-//       segundoApellido: string,
-//       IdTipoIdent: number,
-//       numeroIdentificacion: string,      
-//       email: string,
-//       //emailsecundario: string,
-//       telefonoCelular: number,
-//       telefonoFijo: number,
-//       direccion: string,
-//     ){}
-//   }
+ export class Clientes {
+    constructor(
+      idCliente: number,
+      primerNombre: string,
+      segundoNombre: null | string,
+      primerApellido: string,
+      segundoApellido: string,
+      IdTipoIdent: number,
+      numeroIdentificacion: string,      
+      email: string,
+      //emailsecundario: string,
+      telefonoCelular: number,
+      telefonoFijo: number,
+      direccion: string,
+    ){}
+  }
 
 // export interface Clientes {
 //   IdCliente: number,
--- src/app/services/clientes/cliente.service.spec.ts
+++ src/app/services/clientes/cliente.service.spec.ts
@@ -1,16 +1,16 @@
-// import { TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 
-// import { ClienteService } from './cliente.service';
+import { ClienteService } from './cliente.service';
 
-// describe('ClienteService', () => {
-//   let service: ClienteService;
+describe('ClienteService', () => {
+  let service: ClienteService;
 
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({});
-//     service = TestBed.inject(ClienteService);
-//   });
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(ClienteService);
+  });
 
-//   it('should be created', () => {
-//     expect(service).toBeTruthy();
-//   });
-// });
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
