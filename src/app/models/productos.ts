5cdc0e1962c36f401c7f6c8751db25fced5795e8
proyectucc <daniel.morave@campusucc.edu.co>
2021-05-20 19:08:20 -0500

    lista productos

---
+++ src/app/models/productos.ts
@@ -0,0 +1,12 @@
+export interface productos {
+
+    idProducto:number,
+    nombreCategoria:string,
+    nombreMarca:string,
+    nombreProducto:string,
+    costoCompra:number,
+    costoVenta:number,
+    cantidad:number,
+    descripcion?: string,
+
+}
---
+++ src/app/services/productos/producto.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { productos } from '../../models/productos';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class productoService {
+
+  /**
+   * Crea una nueva instancia de la clase
+   */
+  constructor(private http: HttpClient) {}
+
+  /**
+   * Servicio que contiene el array de empleados
+   */
+  cargarproducto() {
+    const urls = 'https://localhost:44384/api/Productos/MostrarProductos';
+
+    return this.http.get<productos>(urls);
+  }
+
+ /* getSingleEmpleado(id): Observable<OneEmpleado> {
+    const direccion = 'https://localhost:44384/api/Empleados?id=' + id;
+
+    return this.http.get<OneEmpleado>(direccion);
+  }
+
+  putEmpleados(formulario: Empleados): Observable<Empleados>{
+    const direccion = 'https://localhost:44384/api/Empleados?EmpleadoId=';
+
+    return this.http.put<Empleados>(direccion, formulario);
+  } /**/
+}
