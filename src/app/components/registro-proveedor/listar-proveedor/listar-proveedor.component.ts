ce87b9f65fbe8e8dd87ca74e8d8b611733b3e91f
Daniel <daniel.morave@campusucc.edu.co>
2021-05-27 19:13:29 -0500

    Se realizan ajustes en component proveedor

--- src/app/components/registro-proveedor/listar-proveedor/listar-proveedor.component.ts
+++ src/app/components/registro-proveedor/listar-proveedor/listar-proveedor.component.ts
@@ -8,10 +8,10 @@ import { ModalParameters } from '../../modal/models/modal.model';
 @Component({
   selector: 'app-listar-proveedor',
   templateUrl: './listar-proveedor.component.html',
-  styleUrls: ['./listar-proveedor.component.scss']
+  styleUrls: ['./listar-proveedor.component.scss'],
 })
 export class ListarProveedorComponent implements OnInit {
- /**
+  /**
    * Configuracion de dialogo formulario
    */
   public modalParameters: ModalParameters;
@@ -24,55 +24,55 @@ export class ListarProveedorComponent implements OnInit {
   /**
    * Lista de proveedores
    */
-   public prov: Proveedores;
-   /**
-    * Crea una nueva instancia de la clase
-    */
-    constructor(
-     private ProveedorService: ProveedorService,
-     private router: Router)
-     {
-      this.modalParameters = {
-        icon: {
-          isEnable: false,
-          type: 'done',
-        },
-        title: 'Eliminación exitosa',
-        body: 'Se ha eliminado correctamente el registro',
-        centerButton: {
-          name: 'Cerrar',
-          isEnable: true,
-        },
-        centerButtonLogin: {
-          name: 'Cerrar',
-          isEnable: false,
-        },
-      };
-    }
-   /**
-    * Se ejecuta cuando se inicializa el componente
-    */
-   public ngOnInit(): void {
-     this.ProveedorService.cargarProveedores().subscribe((resp) => {
-       this.prov = resp;
-       console.log('DATOS', resp);
-     });
-   }
+  public prov: Proveedores;
+  /**
+   * Crea una nueva instancia de la clase
+   */
+  constructor(
+    private ProveedorService: ProveedorService,
+    private router: Router
+  ) {
+    this.modalParameters = {
+      icon: {
+        isEnable: false,
+        type: 'done',
+      },
+      title: 'Eliminación exitosa',
+      body: 'Se ha eliminado correctamente el registro',
+      centerButton: {
+        name: 'Cerrar',
+        isEnable: true,
+      },
+      centerButtonLogin: {
+        name: 'Cerrar',
+        isEnable: false,
+      },
+    };
+  }
+  /**
+   * Se ejecuta cuando se inicializa el componente
+   */
+  public ngOnInit(): void {
+    this.ProveedorService.cargarProveedores().subscribe((resp) => {
+      this.prov = resp;
+      console.log('DATOS', resp);
+    });
+  }
 
- /**
-    * Método que se ejecuta para la edición de un registro
-    */
+  /**
+   * Método que se ejecuta para la edición de un registro
+   */
   editarProveedor(id) {
-   this.ProveedorService.getSingleProveedor(id).subscribe((data) => {
-     this.router.navigate(['edit-proveedor', data.idProveedor]);
-   });
- }
-   /**
+    this.ProveedorService.getSingleProveedor(id).subscribe((data) => {
+      this.router.navigate(['edit-proveedor', data.idProveedor]);
+    });
+  }
+  /**
    * Método que se ejecuta para la eliminación de un registro
    */
-    eliminarProveedor(id) {
-     // this.ProveedorService.deleteProveedor(id).subscribe();
+  eliminarProveedor(id) {
+    this.ProveedorService.deleteProveedor(id).subscribe();
 
-      this.dialogForm.onShowDialog();
-    }
+    this.dialogForm.onShowDialog();
+  }
 }
